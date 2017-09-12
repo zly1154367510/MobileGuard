399237097f1eb568319f4da054e8e3c39c71b71a
zly1154367510 <1154367510@qq.com>
2017-09-12 18:43:03 +0800

    只是测试

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -1,5 +1,6 @@
 package cn.edu.gdmec.android.mobileguard;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,10 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         if(1==1){
+            System.out.print("asd");
+        }
+
+        for(int i=2;i<=3;i++){
             System.out.print("asd");
         }
 }
