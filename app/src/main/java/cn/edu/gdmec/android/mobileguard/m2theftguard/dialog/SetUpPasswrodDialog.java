20c7d747f5768add93286e026236667e01287970
tong666 <997097287@qq.com>
2017-09-26 20:06:23 +0800

    请从MD5开始

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/dialog/SetUpPasswrodDialog.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.dialog;
+
+import android.app.Dialog;
+import android.view.View;
+
+/**
+ * Created by 通哥 on 2017/9/26.
+ */
+
+public class SetUpPasswrodDialog extends Dialog implements View.OnClickListener {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/utils/MD5Utils.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.utils;
+
+/**
+ * Created by 通哥 on 2017/9/26.
+ */
+
+public class MD5Utils {
+}
